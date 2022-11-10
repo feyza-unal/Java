d267d41aa195e8b9fe6674cb901777542168bbe0
Feyza Unal <feyzaunal95@gmail.com>
2022-11-10 22:25:19 +0300

    Initial commit

--- src/main/java/day28abstraction/ReadFileLineByLine.java
+++ src/main/java/day28abstraction/ReadFileLineByLine.java
@@ -4,7 +4,13 @@ import java.io.*;
 /*
 interview  sorusu !!!
 "throw" ile "throws" arasindaki farklar nelerdir ?
-
+    1)"throw" method'un body'si icinde kullanilir. "throws" ise method'un isminden sonra kullanilir
+    2)"throw" dan sonra onject olusturulur."throws" dan sonra ise sadece Exception Class'ın ismi yazilir.
+    3)"throw" method'un icinde istedigimiz yerde Exception uretmek kullanilir.
+      "throws" ise var olan checked Exception'ı atmak icin kullanilir.
+      "throw" Exception uretir, "throws" varolan Exception'ı atar.
+    4)"throw" dan sonra sadece bir tane Exception olabilir.
+      "throws" dan sonra 1 den fazla Exception olabilir.
 
  */
 public class ReadFileLineByLine {
