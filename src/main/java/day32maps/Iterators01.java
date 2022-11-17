5e39442eaa5e8119c84a39104b09f6cfd764f369
Feyza Unal <feyzaunal95@gmail.com>
2022-11-18 00:24:34 +0300

    Initial commit

---
+++ src/main/java/day32maps/Iterators01.java
@@ -0,0 +1,33 @@
+package day32maps;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class Iterators01 {
+    public static void main(String[] args) {
+        List<String> myList = new ArrayList<>();
+        myList.add("Z");
+        myList.add("K");
+        myList.add("A");
+        myList.add("J");
+        myList.add("M");
+
+        // for-each loop kullanarak elemanin sonuna "!" ekleyiniz
+        //Note: loop'lar kendi baslarina "collection"lari update edemezler. bu yuzden java "iterator"lari olusturdu
+        // iterator lar collectionlari update etmek icin kullanilir
+        for (String w:myList) {
+            w = w+"!";
+        }
+        System.out.println(myList); //[Z, K, A, J, M]
+
+        ListIterator<String> itr = myList.listIterator();
+        while (itr.hasNext()) {
+            String el = itr.next();
+            itr.set(el+"!");
+        }
+        System.out.println(myList); //[Z!, K!, A!, J!, M!]
+
+
+    }
+}
--- src/main/java/day32maps/StaticBlocks01.java
+++ src/main/java/day32maps/StaticBlocks01.java
@@ -4,14 +4,21 @@ public class StaticBlocks01 {
 
     public static double pi;
 
-    //static block'lar ihtiyacimiz olan variable'larin class olusturma safhasinda elimizde olmasini saglar
-
-    static {
-        pi = 3.14;
+    static{
+        System.out.println("Static block 2");
     }
 
-    public static void main(String[] args) {
+    //static block'lar ihtiyacimiz olan variable'larin class olusturma safhasinda elimizde olmasini saglar
+    //static block'lar Class icindeki her seyden once calistirilir. "main method"dan ve diger tum meyhodlardan once calistirilir
+   //static block'lar icinde sadece "static variable" lara deger atanabilir
+    //1'den fazla "static block" varsa ustteki once calistirilir.
 
+    public static void main(String[] args) {
+        System.out.println("Main method");
+    }
+    static{
+        pi=3.14;
+        System.out.println("Static block 1");
     }
 
 
